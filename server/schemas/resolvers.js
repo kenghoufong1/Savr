90d275d189b2d9544561bed9b915cc333c41d873
Patrick <patrick@Patricks-iMac.local>
2023-04-19 20:29:09 -0700

    add auth.js and pull in sign token and authentication error for resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,6 @@
 const { Post, User } = require('../models');
+const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
   Query: {
