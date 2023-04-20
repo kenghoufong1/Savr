3ca4cc099271de9e5745ac217fdd6cff5f2aa967
DESKTOP-G800GM0\uriyu <uriyuri00@gmail.com>
2023-04-19 20:36:32 -0700

    update resolvers, create utils/auth

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,29 +1,66 @@
-const { Tech, Matchup } = require('../models');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
+
 
 const resolvers = {
   Query: {
-    tech: async () => {
-      return Tech.find({});
+    users: async () => {
+      return User.find().populate('posts');
     },
-    matchups: async (parent, { _id }) => {
-      const params = _id ? { _id } : {};
-      return Matchup.find(params);
+    user: async (parent, args) => {
+      const { _id, username } = args;
+      return User.findOne().populate({_id}).populate('posts')
     },
+    me: async ( parent, args, context) => {
+      if (context.user) {
+        return User.findOne({_id: context.user._id }).populate('posts');          
+      }
+      throw new AuthenticationError('You need to be logged in')
+    }
   },
   Mutation: {
-    createMatchup: async (parent, args) => {
-      const matchup = await Matchup.create(args);
-      return matchup;
+    savePost: async ( parent, args, context ) => {
+      if (context.user) {
+        return User.findByIdAndUpdate(
+          { _id: context.user._id},
+          { $addToSet: { savedPosts: args}},
+          { new: true, runValidators: true }
+        )
+      }
     },
-    createVote: async (parent, { _id, techNum }) => {
-      const vote = await Matchup.findOneAndUpdate(
-        { _id },
-        { $inc: { [`tech${techNum}_votes`]: 1 } },
-        { new: true }
-      );
-      return vote;
+    removePost: async ( parent, {postId}, context ) => {
+        console.log(postId);
+
+        return await User.findByIdAndUpdate(
+          { _id: context.user._id},
+          { $pull: { savedPosts: { postId: postId } } },
+          { new: true }
+        );
+      }
     },
-  },
-};
+    adduser: async ( parent, {username, email, password }, context ) => {
+      const user = await User.create({username, email, password });
+      const token = signToken(user);
+
+      return {token, user}
+    },
+    login: async ( parent, { email, password }, context ) => {
+      const user = await User.findOne({email});
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+       const corretPw = await user.isCorrectPassword(password);
+
+       if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+       }
+
+       const token = signToken(user);
+
+       return { token, user }
+    },
+  };
 
 module.exports = resolvers;
