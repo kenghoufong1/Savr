8984875ddf3e27b7d8e79d5e680a6dbdc4b6a1b0
Gene <genedanielkim@gmail.com>
2023-04-20 19:15:33 -0700

    changed App.js back

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink, } from '@apollo/client';
-
-import Home from './pages/Home';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Profile from './pages/Profile';
-import SharedDeals from './pages/SharedDeals';
-
-import Header from './components/Header';
-
+import Login from './pages/Login'
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -19,43 +12,14 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-    <Router>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Routes>
-            <Route 
-              path="/"
-              element={<Home />}
-            />
-            <Route 
-              path="/login" 
-              element={<Login />}
-            />
-            <Route 
-              path="/signup" 
-              element={<Signup />}
-            />
-            <Route 
-              path="/me" 
-              element={<Profile />}
-            />
-            {/* <Route 
-              path="/profiles/:username" 
-              element={<Profile />}
-            /> */}
-            <Route 
-              path="/deals" 
-              element={<SharedDeals />}
-            />
-          </Routes>
-        </div>
-        <Footer />
-      </div>
-    </Router>
-  </ApolloProvider>
-);
-
+      <Router>
+        <Routes>
+          <Route path='/signup' element={<Signup/>}/>
+          <Route path='/login'  element={<Login/>}/>
+        </Routes>
+      </Router>
+    </ApolloProvider>
+  );
 }
 
 export default App;
