78d58f578896a7a37fd2b088a4ec77572a89758b
kenghoufong1 <kenghoufong1@gmail.com>
2023-04-26 21:25:46 -0700

    added the remove button to the myprofile saveddeal part

--- client/src/components/SharedDeals.js
+++ client/src/components/SharedDeals.js
@@ -1,8 +1,35 @@
-import React, { useState } from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Button, Col } from 'react-bootstrap';
+import styled from 'styled-components';
+import { useMutation, useQuery } from '@apollo/client';
+import { SAVE_POST, REMOVE_SAVED_POST } from '../utils/mutations';
+import React, { useState, useEffect } from 'react';
 
+const StyledButton = styled(Button)`
+  margin-bottom: 0;
+`;
 
 function Shareddealcard({ post }) {
+    const [removeSavedPost, { err }] = useMutation(REMOVE_SAVED_POST);
+    const [showCard, setShowCard] = useState(true);
+
+    const handleRemove = async (event) => {
+        console.log(post);
+        try {
+            // mutation
+            const { data } = await removeSavedPost({
+                variables: { postId: _id },
+            });
+            console.log(data.removeSavedPost)
+            setShowCard(false);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    if (!showCard) {
+        return null; // Don't render the card if showCard is false
+    }
+    
     const {
         location,
         product,
@@ -11,7 +38,8 @@ function Shareddealcard({ post }) {
         image,
         description,
         postAuthor,
-      } = post;
+        _id
+    } = post;
     return (
         <Card>
             <Card.Header>Deals</Card.Header>
@@ -19,12 +47,13 @@ function Shareddealcard({ post }) {
             <Card.Body>
                 <Card.Title>{product}</Card.Title>
                 <Card.Text>
-                        <p>Location:{location}</p>
-                        <p>Og Price: ${regPrice.toFixed(2)}</p>
-                        <p>Discounted Price: ${salePrice.toFixed(2)}</p>
-                        <p>Duration of Deal: </p>
-                        <p>Description: {description}.</p>
+                    <p>Location:{location}</p>
+                    <p>Og Price: ${regPrice.toFixed(2)}</p>
+                    <p>Discounted Price: ${salePrice.toFixed(2)}</p>
+                    <p>Duration of Deal: </p>
+                    <p>Description: {description}.</p>
                 </Card.Text>
+                <StyledButton id={_id} variant="primary" onClick={handleRemove}>Remove</StyledButton>
             </Card.Body>
         </Card>
     );
