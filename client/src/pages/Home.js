10cc92e5e25b101f01ccca419f324adbd7743987
miriam <miriamsilvaaa484@gmail.com>
2023-04-27 18:40:07 -0700

    added css to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,7 +4,17 @@ import '../Home.css';
 function Home() {
     return (
         <div className="home-container">
-            <h1 id='welcomemsg'>Welcome to Savr</h1>
+            <div className='hero'>
+                <h1 id='welcomemsg'>Welcome to Savr</h1>
+            </div>
+            <div className='deal-info'>
+                <div>
+                    <img src='assets/savr2.jpg' alt='' />
+                </div>
+                <div className='info-details'>   
+                    <p>Start saving with our website. View and post deals around Seattle.</p>
+                </div>
+            </div>
         </div>
     );
 }
