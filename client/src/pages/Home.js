e1a0f70389109de31c2e186b32b1fe93262e2862
kenghoufong1 <kenghoufong1@gmail.com>
2023-04-27 18:43:23 -0700

    added a button to the front page with some animejs on it

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import { useEffect , useRef } from 'react';
+import anime from 'animejs';
 import '../Home.css';
 
+
 function Home() {
+    const startShoppingBtnRef = useRef(null);
+  
+    useEffect(() => {
+      const startShoppingBtn = startShoppingBtnRef.current;
+      const anim = anime({
+        targets: startShoppingBtn,
+        scale: [1, 1.8],
+        duration: 800,
+        direction: 'alternate',
+        loop: true,
+        easing: 'easeInOutQuad'
+      });
+  
+      return () => {
+        anim.pause();
+      }
+    }, []);
+  
+    
     return (
-        <div className="home-container">
-            <h1 id='welcomemsg'>Welcome to Savr</h1>
-        </div>
+      <div className="home-container">
+        <h1 id='welcomemsg'>Welcome to Savr</h1>
+        <a href="/shareddeal">
+          <button id='mainbutton' ref={startShoppingBtnRef}>Start Shopping</button>
+        </a>
+      </div>
     );
-}
+  }
 
 export default Home
